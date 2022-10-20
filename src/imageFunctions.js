7a572f46c85fa7f1e40af0ba39eab206080af1fe
Craig Loewen <crloewen@microsoft.com>
2022-10-20 15:35:00 -0400

    Image functions now stitches images together

--- src/imageFunctions.js
+++ src/imageFunctions.js
@@ -13,6 +13,146 @@ let mImgStitch = null;
 let mMultiStitchImages = null;
 
 export default {
+    copyOver(source, destination, homographyMat) {
+        // From here: https://stackoverflow.com/questions/13063201/how-to-show-the-whole-image-when-using-opencv-warpperspective
+        // '''warp img2 to img1 with homograph H'''
+        // h1,w1 = img1.shape[:2]
+        // h2,w2 = img2.shape[:2]
+        // pts1 = float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2)
+        // pts2 = float32([[0,0],[0,h2],[w2,h2],[w2,0]]).reshape(-1,1,2)
+        // pts2_ = cv2.perspectiveTransform(pts2, H)
+        // pts = concatenate((pts1, pts2_), axis=0)
+        // [xmin, ymin] = int32(pts.min(axis=0).ravel() - 0.5)
+        // [xmax, ymax] = int32(pts.max(axis=0).ravel() + 0.5)
+        // t = [-xmin,-ymin]
+        // Ht = array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) # translate
+
+        // result = cv2.warpPerspective(img2, Ht.dot(H), (xmax-xmin, ymax-ymin))
+        // result[t[1]:h1+t[1],t[0]:w1+t[0]] = img1
+        // return result
+
+        // Rewrite the above code in Javascript
+        const h1 = source.rows;
+        const w1 = source.cols;
+        const h2 = destination.rows;
+        const w2 = destination.cols;
+
+        const pts1 = new cv.Mat(4, 1, cv.CV_32FC2);
+        const pts2 = new cv.Mat(4, 1, cv.CV_32FC2);
+
+        pts1.data32F[0] = 0;
+        pts1.data32F[1] = 0;
+        pts1.data32F[2] = 0;
+        pts1.data32F[3] = h1;
+
+        pts1.data32F[4] = w1;
+        pts1.data32F[5] = h1;
+        pts1.data32F[6] = w1;
+        pts1.data32F[7] = 0;
+
+        pts2.data32F[0] = 0;
+        pts2.data32F[1] = 0;
+        pts2.data32F[2] = 0;
+        pts2.data32F[3] = h2;
+
+        pts2.data32F[4] = w2;
+        pts2.data32F[5] = h2;
+        pts2.data32F[6] = w2;
+        pts2.data32F[7] = 0;
+
+        const pts2_ = new cv.Mat();
+        cv.perspectiveTransform(pts2, pts2_, homographyMat);
+
+        // Reshape pts1 and pts2_ to x and y vectors
+        const pts1MatVector = new cv.MatVector();
+        cv.split(pts1, pts1MatVector);
+
+        const pts2MatVector = new cv.MatVector();
+        cv.split(pts2_, pts2MatVector);
+
+        // Get value of pts1_ at index 0
+        const pts1_x = pts1MatVector.get(0);
+        const pts1_y = pts1MatVector.get(1);
+
+        const pts2_x = pts2MatVector.get(0);
+        const pts2_y = pts2MatVector.get(1);
+
+        // Concatenate pts1 and pts2
+        const XPtsVector = new cv.MatVector();
+        XPtsVector.push_back(pts1_x);
+        XPtsVector.push_back(pts2_x);
+
+        const YPtsVector = new cv.MatVector();
+        YPtsVector.push_back(pts1_y);
+        YPtsVector.push_back(pts2_y);
+
+        const allXPts = new cv.Mat();
+        cv.vconcat(XPtsVector, allXPts);
+
+        const allYPts = new cv.Mat();
+        cv.vconcat(YPtsVector, allYPts);
+
+        let xMinMax = cv.minMaxLoc(allXPts);
+        let yMinMax = cv.minMaxLoc(allYPts);
+
+        console.log(xMinMax);
+
+        const xmin = xMinMax.minVal; 
+        const ymin = yMinMax.minVal;
+
+        const xmax = xMinMax.maxVal;
+        const ymax = yMinMax.maxVal;
+
+        const t = [-xmin, -ymin];
+
+        const Ht = new cv.Mat(3, 3, cv.CV_64FC1);
+        Ht.data64F[0] = 1;
+        Ht.data64F[1] = 0;
+        Ht.data64F[2] = t[0];
+
+        Ht.data64F[3] = 0;
+        Ht.data64F[4] = 1;
+        Ht.data64F[5] = t[1];
+
+        Ht.data64F[6] = 0;
+        Ht.data64F[7] = 0;
+        Ht.data64F[8] = 1;
+
+        // Cross multiply Ht and homographyMat
+        let translatedHomographyMat = new cv.Mat();
+        cv.gemm(Ht, homographyMat, 1, new cv.Mat(), 0, translatedHomographyMat);
+
+        const result = new cv.Mat();
+        cv.warpPerspective(destination, result, translatedHomographyMat, new cv.Size(xmax - xmin, ymax - ymin));
+
+        const roi = new cv.Rect(t[0], t[1], w1, h1);
+        const destinationROI = result.roi(roi);
+        source.copyTo(destinationROI);
+
+        // Get four points that are the corners of destination
+        const corners = new cv.Mat(4, 1, cv.CV_32FC2);
+        corners.data32F[0] = 0;
+        corners.data32F[1] = 0;
+        corners.data32F[2] = 0;
+        corners.data32F[3] = h2;
+
+        corners.data32F[4] = w2;
+        corners.data32F[5] = h2;
+        corners.data32F[6] = w2;
+        corners.data32F[7] = 0;
+
+        // Transform the corners of destination to the new coordinate system
+        let cornersTransformed = new cv.Mat(4, 1, cv.CV_32FC2);
+        cv.perspectiveTransform(corners, cornersTransformed, translatedHomographyMat);
+
+        // // Draw the new corners on result
+        cv.line(result, {x: cornersTransformed.data32F[0], y: cornersTransformed.data32F[1]}, {x: cornersTransformed.data32F[2], y: cornersTransformed.data32F[3]}, new cv.Scalar(0, 255, 0, 255), 2);
+        cv.line(result, {x: cornersTransformed.data32F[2], y: cornersTransformed.data32F[3]}, {x: cornersTransformed.data32F[4], y: cornersTransformed.data32F[5]}, new cv.Scalar(0, 255, 0, 255), 2);
+        cv.line(result, {x: cornersTransformed.data32F[4], y: cornersTransformed.data32F[5]}, {x: cornersTransformed.data32F[6], y: cornersTransformed.data32F[7]}, new cv.Scalar(0, 255, 0, 255), 2);
+        cv.line(result, {x: cornersTransformed.data32F[6], y: cornersTransformed.data32F[7]}, {x: cornersTransformed.data32F[0], y: cornersTransformed.data32F[1]}, new cv.Scalar(0, 255, 0, 255), 2);
+
+        return result;
+    },
     doCVStuff(mainImage, toAlignImage) {
         // Implementation used from this blog: https://scottsuhy.com/2021/02/01/image-alignment-feature-based-in-opencv-js-javascript
 
@@ -20,18 +160,18 @@ export default {
         let im2Input = cv.matFromImageData(toAlignImage);
 
         //im2 is the original reference image we are trying to align to
-        let im2 = cv.matFromImageData(mainImage);
+        let mainImageMat = cv.matFromImageData(mainImage);
         //im1 is the image we are trying to line up correctly
-        let im1 = cv.matFromImageData(toAlignImage);
+        let toAlignImageMat = cv.matFromImageData(toAlignImage);
 
         //17            Convert images to grayscale
         //18            Mat im1Gray, im2Gray;
         //19            cvtColor(im1, im1Gray, CV_BGR2GRAY);
         //20            cvtColor(im2, im2Gray, CV_BGR2GRAY);
-        let im1Gray = new cv.Mat();
-        let im2Gray = new cv.Mat();
-        cv.cvtColor(im1, im1Gray, cv.COLOR_BGRA2GRAY);
-        cv.cvtColor(im2, im2Gray, cv.COLOR_BGRA2GRAY);
+        let toAlignImageGray = new cv.Mat();
+        let mainImageGray = new cv.Mat();
+        cv.cvtColor(toAlignImageMat, toAlignImageGray, cv.COLOR_BGRA2GRAY);
+        cv.cvtColor(mainImageMat, mainImageGray, cv.COLOR_BGRA2GRAY);
 
         //22            Variables to store keypoints and descriptors
         //23            std::vector<KeyPoint> keypoints1, keypoints2;
@@ -46,8 +186,8 @@ export default {
         //28            orb->detectAndCompute(im1Gray, Mat(), keypoints1, descriptors1);
         //29            orb->detectAndCompute(im2Gray, Mat(), keypoints2, descriptors2);
         var orb = new cv.ORB(5000);
-        orb.detectAndCompute(im1Gray, new cv.Mat(), keypoints1, descriptors1);
-        orb.detectAndCompute(im2Gray, new cv.Mat(), keypoints2, descriptors2);
+        orb.detectAndCompute(toAlignImageGray, new cv.Mat(), keypoints1, descriptors1);
+        orb.detectAndCompute(mainImageGray, new cv.Mat(), keypoints2, descriptors2);
 
         //31            Match features.
         //32            std::vector<DMatch> matches;
@@ -75,7 +215,7 @@ export default {
         //47            imwrite("matches.jpg", imMatches);
         let imMatches = new cv.Mat();
         let color = new cv.Scalar(0, 255, 0, 255);
-        cv.drawMatches(im1, keypoints1, im2, keypoints2, good_matches, imMatches, color);
+        cv.drawMatches(toAlignImageMat, keypoints1, mainImageMat, keypoints2, good_matches, imMatches, color);
 
         //50            Extract location of good matches
         //51            std::vector<Point2f> points1, points2;
@@ -91,64 +231,27 @@ export default {
         for (let i = 0; i < good_matches.size(); i++) {
             let keyPoint1 = keypoints1.get(good_matches.get(i).queryIdx);
             let keyPoint2 = keypoints2.get(good_matches.get(i).trainIdx);
-            console.log("Keypoint 1: ", keyPoint1);
             points1.push(keyPoint1.pt.x);
             points1.push(keyPoint1.pt.y);
             points2.push(keyPoint2.pt.x);
             points2.push(keyPoint2.pt.y);
         }
 
-        //59            Find homography
-        //60            h = findHomography( points1, points2, RANSAC );
-        //The first 2 arguments to findHomography need to be matArray so you must convert your point1 and point2 to matArray
-        //reference: https://docs.opencv.org/3.4/d9/d0c/group__calib3d.html#ga4abc2ece9fab9398f2e560d53c8c9780
-
-        // points1 = [10, 10, 20, 10, 10, 20, 20, 20]
-
-        // Make points2 a translation of points1 by (-10,-10) and a scale of 1/2
-        // points2 = [0, 0, 5, 0, 0, 5, 5, 5]
-
         var mat1 = new cv.Mat(points1.length / 2, 1, cv.CV_32FC2);
         mat1.data32F.set(points1);
         var mat2 = new cv.Mat(points2.length / 2, 1, cv.CV_32FC2);
         mat2.data32F.set(points2);
-        let h = cv.findHomography(mat1, mat2, cv.RANSAC);
+        let homographyMat = cv.findHomography(mat1, mat2, cv.RANSAC);
 
-        console.log(mat1);
-        console.log(mat2);
-        console.log(h);
+        let warpedPerspectiveImage = new cv.Mat();
+        cv.warpPerspective(toAlignImageMat, warpedPerspectiveImage, homographyMat, mainImageMat.size());
 
-        //62          Use homography to warp image
-        //63          warpPerspective(im1, im1Reg, h, im2.size());
-        let image_B_final_result = new cv.Mat();
-        cv.warpPerspective(im1, image_B_final_result, h, im2.size());
+        let blendedImage = new cv.Mat();
+        cv.addWeighted(mainImageMat, 0.5, warpedPerspectiveImage, 0.5, 0, blendedImage);
 
-        // Create a new image that has both im2 and image_B_final_result
-        let image_A_final_result = new cv.Mat();
-        cv.addWeighted(im2, 0.5, image_B_final_result, 0.5, 0, image_A_final_result);
 
-        // Get four points that are the corners of im1
-        let corners = new cv.Mat(4, 1, cv.CV_32FC2);
-        corners.data32F[0] = 0;
-        corners.data32F[1] = 0;
-        corners.data32F[2] = im1.cols;
-        corners.data32F[3] = 0;
-        corners.data32F[4] = im1.cols;
-        corners.data32F[5] = im1.rows;
-        corners.data32F[6] = 0;
-        corners.data32F[7] = im1.rows;
+        let combinedImage = this.copyOver(mainImageMat, toAlignImageMat,homographyMat);
 
-        // Transform the corners of im2 to the corners of im1
-        let cornersTransformed = new cv.Mat(4, 1, cv.CV_32FC2);
-        cv.perspectiveTransform(corners, cornersTransformed, h);
-
-        // Draw the new corners on image_B_final_result
-        cv.line(image_A_final_result, {x: cornersTransformed.data32F[0], y: cornersTransformed.data32F[1]}, {x: cornersTransformed.data32F[2], y: cornersTransformed.data32F[3]}, new cv.Scalar(0, 255, 0, 255), 2);
-        cv.line(image_A_final_result, {x: cornersTransformed.data32F[2], y: cornersTransformed.data32F[3]}, {x: cornersTransformed.data32F[4], y: cornersTransformed.data32F[5]}, new cv.Scalar(0, 255, 0, 255), 2);
-        cv.line(image_A_final_result, {x: cornersTransformed.data32F[4], y: cornersTransformed.data32F[5]}, {x: cornersTransformed.data32F[6], y: cornersTransformed.data32F[7]}, new cv.Scalar(0, 255, 0, 255), 2);
-        cv.line(image_A_final_result, {x: cornersTransformed.data32F[6], y: cornersTransformed.data32F[7]}, {x: cornersTransformed.data32F[0], y: cornersTransformed.data32F[1]}, new cv.Scalar(0, 255, 0, 255), 2);
-
-        // Turn imMatches mat into ImageData
-        return [imMatches, image_A_final_result];
+        return [imMatches, combinedImage];
     }
 }
