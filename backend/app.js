fa8acc9d917d05d43ad8b34614bf794e794e24d5
Craig Loewen <crloewen@microsoft.com>
2022-11-28 12:20:00 -0500

    Fixed deploy

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,10 @@ let hostPort = 3000;
 
 if (process.env.NODE_ENV == "production") {
     hostPort = 80;
+    console.log("Running as production!");
 } else {
     hostPort = process.env.PORT || 3000;
+    console.log("Running as development!");
 }
 
 const port = hostPort;
